cb3aa166fb384632012e8b5f851fb094890f3d1d
Microsystemty <mateusalessandro250@gmail.com>
2025-05-16 17:18:13 -0300

    Project new pages

--- sw.js
+++ sw.js
@@ -1,16 +1,1 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('controle-clientes-cache').then(cache => {
-    return cache.addAll([
-      './',
-      './index.html',
-      './dashboard.html',
-      './app.js',
-      './dashboard.js',
-      './style.css',
-      './manifest.json'
-    ]);
-  }));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+// Service worker para PWA
